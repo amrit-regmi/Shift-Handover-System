0677bfa52332be26a6ed6f965f8489a722868960
amrit <rit.regmi@gmail.com>
2020-12-07 16:42:08 +0200

    Added support for direct routes

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,25 @@ const app = express()
 
 const schema = makeExecutableSchema({ typeDefs, resolvers, schemaDirectives: { constraint: ConstraintDirective }})
 
-const server = new ApolloServer({schema:schema, context: async({req}) => {
-  const auth = req? req.headers.authorization : null
-  if (auth && auth.toLocaleLowerCase().startsWith('bearer')){
-    const token = jwt.verify(auth.substring(7), config.JWT_SECRET)
-    const currentStation = await Station.findById(token.stationId)
-    const currentUser = await Staff.findById(token.id).populate({ path:'permission' })
-    return {currentStation,currentUser,}
-  }
-}})
+const server = new ApolloServer(
+  {
+    schema:schema,
+     context: async({req}) => {
+      const auth = req? req.headers.authorization : null
+      if (auth && auth.toLocaleLowerCase().startsWith('bearer')){
+        const token = jwt.verify(auth.substring(7), config.JWT_SECRET)
+        const currentStation = await Station.findById(token.stationId)
+        const currentUser = await Staff.findById(token.id).populate({ path:'permission' })
+        return {currentStation,currentUser,}
+      }
+    }
+  })
 
 app.use(express.static('build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+})
   
 server.applyMiddleware({ app,path:'/' })
 
@@ -31,7 +39,6 @@ app.listen({ port: config.PORT }, () => {
   console.log('Apollo Server on http://localhost:'+config.PORT );
 })
 
-  
 mongoose.set('useFindAndModify', false)
 const mongoUrl = config.MONGODB_URI
 

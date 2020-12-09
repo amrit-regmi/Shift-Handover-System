68be17cabf5e0d863ab8abcfade56a97108053f1
amrit <rit.regmi@gmail.com>
2020-12-09 23:55:57 +0200

    Modified navigation path - now navigation path is set relative to current page

--- frontend/src/components/StationInfo/stationMenu.js
+++ frontend/src/components/StationInfo/stationMenu.js
@@ -1,21 +1,36 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
-const StationMenu = ({ stationLocation ,activeItem, setActiveItem }) => {
+const StationMenu = ({ station ,activeItem, setActiveItem }) => {
   const history = useHistory()
 
+  const location = useLocation()
+
+  const navigatePath = (page) => {
+    const aar = location.pathname.split('/').filter(path => path !== '')
+
+    const navPath = aar.reduce((p,c,i) => {
+      if(c === activeItem || aar.length- 1 === i){ //If the current item on iteration is current page exit reduce
+        aar.splice(1)
+        return p
+      }
+      return p+'/'+c
+    },'')
+
+    return navPath+'/'+page
+  }
 
   const handleMenuClick = (e, { name }) => {
     setActiveItem(name)
     if(activeItem !== name){
-      history.push(name)
+      history.push(navigatePath(name))
     }
 
   }
   return (
     <Menu pointing secondary >
-      <Menu.Item header>{stationLocation}</Menu.Item>
+      <Menu.Item header>{station.location}</Menu.Item>
       <Menu.Item
         position='right'
         name='BasicInfo'

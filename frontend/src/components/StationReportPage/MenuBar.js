f7f5033778fe83898e97273710216fbb3dea9f42
amrit <rit.regmi@gmail.com>
2020-12-22 22:21:49 +0200

    If user now navigates away from active reporting shows data loss warning

--- frontend/src/components/StationReportPage/MenuBar.js
+++ frontend/src/components/StationReportPage/MenuBar.js
@@ -1,11 +1,48 @@
-import React from 'react'
-import { Button, Menu } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Confirm, Menu } from 'semantic-ui-react'
 import { useHistory } from 'react-router-dom'
 
 
 const MenuBar = ({ activeItem, setActiveItem }) => {
   const history = useHistory()
-  const handleMenuClick = (e, { name }) => setActiveItem( name )
+  const [confirmOpen,setConfirmOpen] = useState(false)
+
+  const [navigatingTo, setNavigatingTo] = useState('')
+
+  const CurrentDataLossWarning = () => {
+    return (
+      <Confirm
+        open = {confirmOpen}
+        cancelButton = 'I understand that all reporting will be lost, Continue'
+        confirmButton ='Cancel and Continue Reporting'
+        header='Are You sure?'
+        content= 'You are navigationg away from from the reporting page, doing so will result in losing all the inputs on the report page. Do you want to continue?'
+        onCancel = {() => {
+          setActiveItem(navigatingTo)
+          setConfirmOpen(false)
+        }}
+
+        onConfirm= {() => {
+          setConfirmOpen(false)
+        }}
+      >
+      </Confirm>
+
+    )
+
+  }
+  const handleMenuClick = (e, { name }) => {
+
+    if(activeItem === 'startNewReport'){
+      setNavigatingTo(name)
+      setConfirmOpen(true)
+      return
+    }
+
+    setActiveItem( name )
+    //history.push(activeItem)
+  }
+
   const switchStation = () => {
     localStorage.removeItem('stationKey')
     sessionStorage.removeItem('stationKey')
@@ -13,36 +50,40 @@ const MenuBar = ({ activeItem, setActiveItem }) => {
   }
 
   return (
-    <Menu inverted color="blue" stackable >
-      <Menu.Item header>  Shift Reporting System </Menu.Item>
-      <Menu.Item name= "lastShiftReport"
-        active = {activeItem === 'lastShiftReport'}
-        onClick = {handleMenuClick}>
-      </Menu.Item>
+    <>
+      <Menu inverted color="blue" stackable >
+        <Menu.Item header>  Shift Reporting System </Menu.Item>
+        <Menu.Item name= "lastShiftReport"
+          active = {activeItem === 'lastShiftReport'}
+          onClick = {handleMenuClick}>
+        </Menu.Item>
 
-      <Menu.Item name= "browseAllReports"
-        active = {activeItem === 'browseAllReports'}
-        onClick = {handleMenuClick}>
-      </Menu.Item>
+        <Menu.Item name= "browseAllReports"
+          active = {activeItem === 'browseAllReports'}
+          onClick = {handleMenuClick}>
+        </Menu.Item>
 
-      <Menu.Item name= "startNewReport"
-        active = {activeItem === 'startNewReport'}
-        onClick = {handleMenuClick}>
-      </Menu.Item>
+        <Menu.Item name= "startNewReport"
+          active = {activeItem === 'startNewReport'}
+          onClick = {handleMenuClick}>
+        </Menu.Item>
 
-      <Menu.Item name= "allOpenTasks"
-        active = {activeItem === 'allOpenTasks'}
-        onClick = {handleMenuClick}>
-      </Menu.Item>
+        { /* Will be implemented on future
+       <Menu.Item name= "allOpenTasks"
+          active = {activeItem === 'allOpenTasks'}
+          onClick = {handleMenuClick}>
+  </Menu.Item>*/}
 
-      <Menu.Menu position='right'>
-        <Button primary
-          name='Switch Station '
-          active={activeItem === 'logout'}
-          onClick={switchStation}
-        >Switch Station</Button>
-      </Menu.Menu>
-    </Menu>
+        <Menu.Menu position='right'>
+          <Button primary
+            name='Switch Station '
+            active={activeItem === 'logout'}
+            onClick={switchStation}
+          >Switch Station</Button>
+        </Menu.Menu>
+      </Menu>
+      <CurrentDataLossWarning/>
+    </>
 
 
 

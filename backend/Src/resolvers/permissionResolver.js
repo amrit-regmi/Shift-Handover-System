9ff2284103526f4df2bd15f147327047e87d3caf
amrit <rit.regmi@gmail.com>
2020-12-24 21:46:41 +0200

    Fixed issue where permission was being overrireden with last updated entry

--- backend/Src/resolvers/permissionResolver.js
+++ backend/Src/resolvers/permissionResolver.js
@@ -5,24 +5,27 @@ const _  = require('lodash')
 const permissionResolver = {
   Mutation: {
     changePermission: async (root,args,context) => {
-      const { id,...update }= { ...args }
+      const { id,...updates }= { ...args }
 
       const loggedInStaff = context.currentUser
-      let permission
+
 
       /**If user is not logged in */
       if(!loggedInStaff){
         throw new AuthenticationError('User must logged in with appropriate permissions')
       }
 
+      const permission = await Permission.findById(id)
+
       /**User cannot assign permission to himself */
-      if(id === loggedInStaff.id){
+      if(permission.staffId === loggedInStaff.id){
         throw new AuthenticationError('User cannot  assign permission to himself')
       }
       /**Only user with admin permession can set admin permsission*/
-      if(update.admin){
+      if(updates.admin){
         if(loggedInStaff.permission && loggedInStaff.permission.admin){
-          permission = await Permission.findByIdAndUpdate(id,update, { new:true }). populate( { path: 'station.edit timesheet.edit timesheet.view timesheet.sign' ,model:'Station' ,select:'id location' })
+          permission.admin = true
+          await permission.save()
           return permission
         }else{
           throw new AuthenticationError('User do not have permission for this action')
@@ -31,7 +34,11 @@ const permissionResolver = {
 
       /**User with admin permession can change any permsission*/
       if(loggedInStaff.permission && loggedInStaff.permission.admin){
-        permission = await Permission.findByIdAndUpdate(id,update, { new:true }). populate( { path: 'station.edit timesheet.edit timesheet.view timesheet.sign' ,model:'Station' ,select:'id location' })
+        permission.staff={ ...permission.staff,...updates.staff }
+        permission.timesheet ={ ...permission.timesheet,...updates.timesheet }
+        permission.station = { ...permission.station,...updates.station }
+        await permission.save()
+        await Permission.populate(permission, { path: 'station.edit timesheet.edit timesheet.view timesheet.sign' ,model:'Station' ,select:'id location' })
         return permission
       }
 
@@ -44,51 +51,53 @@ const permissionResolver = {
         /** check if user is trying to assign permissions that user don't have */
 
         /**If station permission is set*/
-        if(update.station){
-          if(update.station.edit && !_.difference(update.station.edit,loggedInStaff.permission.station.edit).length){
+        if(updates.station){
+          if(updates.station.edit && !_.difference(updates.station.edit,loggedInStaff.permission.station.edit).length){
             throw new AuthenticationError('User do not have right to assign edit permission for provided station')
           }
-          if(update.station.add  && ! loggedInStaff.permission.station.edit){
+          if(updates.station.add  && ! loggedInStaff.permission.station.edit){
             throw new AuthenticationError('User do not have right to assign add station permission')
           }
+
+
+
+          permission. station = { ...permission.station,...updates.stations }
         }
 
         /**If timesheet permission is set*/
-        if(update.timesheet){
-          if(update.timesheet.edit && !_.difference(update.timesheet.edit,loggedInStaff.permission.timesheet.edit).length){
-            throw new AuthenticationError('User do not have right to assign timesheet edit permission for provided station')
+        if(updates.timesheet){
+          if(updates.timesheet.sign && !_.difference(updates.timesheet.edit,loggedInStaff.permission.timesheet.sign).length){
+            throw new AuthenticationError('User do not have right to assign timesheet sign permission for provided station')
           }
-          if(update.timesheet.view && !_.difference(update.timesheet.view,loggedInStaff.permission.timesheet.view).length){
+          if(updates.timesheet.view && !_.difference(updates.timesheet.view,loggedInStaff.permission.timesheet.view).length){
             throw new AuthenticationError('User do not have right to assign timesheet view permission for provided station')
           }
 
-          console.log(update)
-          if(update.station.sign && !_.difference(update.timesheet.sign,loggedInStaff.permission.timesheet.sign).length){
-            throw new AuthenticationError('User do not have right to assign timesheet view permission for provided station')
-          }
+          permission.timesheet ={ ...permission.timesheet,...updates.timesheet }
+
         }
 
         /**If staff permission is set*/
-        if(update.staff){
-          if(update.staff.edit  && ! loggedInStaff.permission.staff.edit){
+        if(updates.staff){
+          if(updates.staff.edit  && ! loggedInStaff.permission.staff.edit){
             throw new AuthenticationError('User do not have right to assign edit staff permission')
 
           }
 
-          if(update.staff.view  && ! loggedInStaff.permission.staff.view){
+          if(updates.staff.view  && ! loggedInStaff.permission.staff.view){
             throw new AuthenticationError('User do not have right to assign view staff permission')
           }
 
-          if(update.staff.add  && ! loggedInStaff.permission.staff.add){
+          if(updates.staff.add  && ! loggedInStaff.permission.staff.add){
             throw new AuthenticationError('User do not have right to assign add staff permission')
           }
+
+          permission.staff={ ...permission.staff,...updates.staff }
         }
 
 
-
-
-
-        permission = await Permission.findByIdAndUpdate(id,update, { new:true }). populate( { path: 'station.edit timesheet.edit timesheet.view timesheet.sign' ,model:'Station' ,select:'id location' })
+        await permission.save()
+        await Permission.populate(permission, { path: 'station.edit timesheet.edit timesheet.view timesheet.sign' ,model:'Station' ,select:'id location' })
         return permission
 
       }

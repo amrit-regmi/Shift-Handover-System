5610f5252690d939015e4ffc95b9ab4aa9e810f1
amrit <rit.regmi@gmail.com>
2021-01-02 00:03:26 +0200

    Added try catch on post hook for populate

--- backend/Src/models/ShiftReport.js
+++ backend/Src/models/ShiftReport.js
@@ -43,39 +43,46 @@ const shiftReportSchema = new mongoose.Schema({
 
 shiftReportSchema.post(['save','findOne', 'find'] ,{ query:true,document:true } ,async function(docs){
   const populateFn = async (doc) => {
-    await doc.populate(
-      [
-        {
-          path:'station',
-          select: ['id','location']
-        },
-        {
-          path:'staffAndTime',
-          select:['id','endTime','startTime', 'staffAndTime'],
-          populate:{
-            path:'staff',
-            select: ['name']
+    try {
+      await doc.populate(
+        [
+          {
+            path:'station',
+            select: ['id','location']
           },
-
-        },
-        {
-          path:'tasks' ,
-          select:['id','aircraft','taskCategory', 'description', 'status', 'updates'],
-          populate:{
-            path:'aircraft updates',
-            select:['registration','id','costumer','action','handoverId','note'],
+          {
+            path:'staffAndTime',
+            select:['id','endTime','startTime', 'staffAndTime'],
             populate:{
-              path: 'costumer handoverId',
-              select:['name','id','shift' ,'station' ,'startTime'],
-              populate:{
-                path: 'station',
-                select:['id', 'location'],
-              }
-            }
+              path:'staff',
+              select: ['name']
+            },
+
           },
-        }
-      ]
-    ).execPopulate()
+          {
+            path:'tasks' ,
+            select:['id','aircraft','taskCategory', 'description', 'status', 'updates'],
+            populate:{
+              path:'aircraft updates',
+              select:['registration','id','costumer','action','handoverId','note'],
+              populate:{
+                path: 'costumer handoverId',
+                select:['name','id','shift' ,'station' ,'startTime'],
+                populate:{
+                  path: 'station',
+                  select:['id', 'location'],
+                }
+              }
+            },
+          }
+        ]
+      ).execPopulate()
+    } catch(e){
+      /**Throw Error except for null document */
+      if(e.message !== 'Cannot read property \'populate\' of null'){
+        throw new Error(e.message)
+      }
+    }
   }
 
   if(isArray(docs)){
